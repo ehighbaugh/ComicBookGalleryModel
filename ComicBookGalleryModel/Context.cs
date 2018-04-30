6b039c884ed97977ec0a932f60940d92331cfaad
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-30 02:02:24 -0400

    created ComicBooks entity class, context class

---
+++ ComicBookGalleryModel/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ComicBookGalleryModel.Models;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
